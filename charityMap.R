020c0686c8e1015d6b6e7b0858ffe9db258a2556
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-07-18 14:17:23 +0100

    finish charity map

--- charityMap.R
+++ charityMap.R
@@ -16,14 +16,26 @@ charityMapServer <- function(id, charities_data_subset) {
     output$charity_map <- renderLeaflet({
 
       # only plot the charities where the contact info is within the chosen LTLA
-      charities_data_subset() |>
+      charities_for_map <- charities_data_subset() |>
         dplyr::filter(flag_contact_in_ltla == TRUE) |>
+        # avoided replace_na() as from tidyr (package not used elsewhere yet)
+        mutate_at(c("charity_contact_web", "charity_contact_email", "charity_contact_phone", "charity_activities"),  ~replace(., is.na(.), "-")) 
+      
+      charities_for_map |>
         leaflet() |>
         addProviderTiles(providers$CartoDB.Positron) |>
-        addMarkers(~long,
-          ~lat,
-          popup = ~charity_name,
-          clusterOptions = markerClusterOptions()
+        addCircleMarkers(~long,
+                          ~lat,
+                          label = ~charity_name,
+                          clusterOptions = markerClusterOptions(),
+                         popup = paste0(
+                           "<b> Name: </b>", charities_for_map$charity_name, "<br>",
+                           "<b> Web: </b>", charities_for_map$charity_contact_web, "<br>",
+                           "<b> Email: </b>", charities_for_map$charity_contact_email, "<br>",
+                           "<b> Phone: </b>", charities_for_map$charity_contact_phone, "<br>",
+                           "<b> Actvities: </b>", charities_for_map$charity_activities
+                         )
+                         
         )
     })
   })
@@ -33,29 +45,29 @@ charityMapServer <- function(id, charities_data_subset) {
 # Test -------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
 
-charities_ltla_lookup <- read_rds("data/charities_ltla_lookup.rds")
-charities_data <- read_rds("data/charities_list_latlong.rds")
-
-source("subsetCharityData.R")
-
-charityMapTest <- function() {
-  ui <- fluidPage(
-    charityMapUI("test"),
-  )
-
-  server <- function(input, output, session) {
-    charities_subset_test <- subsetCharityDataServer(
-      "test",
-      charities_data = charities_data,
-      charities_ltla_lookup_data = charities_ltla_lookup,
-      ltlas_for_filtering = reactive(c("E08000014"))
-    )
-
-    charityMapServer("test",
-      charities_data_subset = charities_subset_test
-    )
-  }
-  shinyApp(ui, server)
-}
-
-charityMapTest()
+# charities_ltla_lookup <- read_rds("data/charities_ltla_lookup.rds")
+# charities_data <- read_rds("data/charities_list_latlong.rds")
+# 
+# source("subsetCharityData.R")
+# 
+# charityMapTest <- function() {
+#   ui <- fluidPage(
+#     charityMapUI("test"),
+#   )
+# 
+#   server <- function(input, output, session) {
+#     charities_subset_test <- subsetCharityDataServer(
+#       "test",
+#       charities_data = charities_data,
+#       charities_ltla_lookup_data = charities_ltla_lookup,
+#       ltlas_for_filtering = reactive(c("E08000014"))
+#     )
+# 
+#     charityMapServer("test",
+#       charities_data_subset = charities_subset_test
+#     )
+#   }
+#   shinyApp(ui, server)
+# }
+# 
+# charityMapTest()
