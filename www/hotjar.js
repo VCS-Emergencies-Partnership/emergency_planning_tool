dbbb7a017120b50a0de20b92dc65219412d7e519
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-09-01 13:38:22 +0100

    fix hotjar script

--- www/hotjar.js
+++ www/hotjar.js
@@ -1,6 +1,4 @@
-<!-- Hotjar Tracking Code for https://vscep.shinyapps.io/emergency_planning_tool/ -->
-<script>
-    (function(h,o,t,j,a,r){
+(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3132927,hjsv:6};
         a=o.getElementsByTagName('head')[0];
@@ -8,4 +6,3 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
