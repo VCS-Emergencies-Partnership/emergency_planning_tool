0c26ac29d85e2ce802bd983e64047aad365f24a6
VictoriaLatham <victorialatham@redcross.org.uk>
2023-02-13 16:39:51 +0000

    spelling error

--- R/explanation.R
+++ R/explanation.R
@@ -4,7 +4,7 @@ explanation <- function() {
       1,
       align = "left",
       tags$div(
-        style = "width:50px",
+        style = "width:80px",
         class = "vcsep-logo",
         tags$a(
           href = "https://vcsep.org.uk/",
@@ -27,7 +27,7 @@ explanation <- function() {
     column(
       11,
       tags$p(
-        "The aim of this tool is to hightlight neighbourhoods vulnerable to potential emergency events, bringing together organisations and resources. We want to support organisations to make more informed decisions when planning for emergencies."
+        "The aim of this tool is to highlight neighbourhoods vulnerable to potential emergency events, bringing together organisations and resources. We want to support organisations to make more informed decisions when planning for emergencies."
         ),
       tags$p(
         "Select an area of interest from the map or the dropdown box and then press next to navigate between the tabs."
