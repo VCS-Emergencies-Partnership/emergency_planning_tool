2dab2582ab5dacdaf4e35074bb92cc8bbe0726f0
VictoriaLatham <victorialatham@redcross.org.uk>
2023-03-22 12:13:33 +0000

    removed production code from footer and button page

--- www/onetrust_footer.js
+++ www/onetrust_footer.js
@@ -1,11 +1,3 @@
-<!-- OneTrust Cookies Consent Notice start for vcsep.shinyapps.io/emergency_planning_tool/ -->
-<script type="text/javascript" src="https://cdn-ukwest.onetrust.com/consent/ce02c104-f04f-4b93-821f-aa1f2cf3e70a/OtAutoBlock.js" ></script>
-<script src="https://cdn-ukwest.onetrust.com/scripttemplates/otSDKStub.js"  type="text/javascript" charset="UTF-8" data-domain-script="ce02c104-f04f-4b93-821f-aa1f2cf3e70a" ></script>
-<script type="text/javascript">
-function OptanonWrapper() { }
-</script>
-<!-- OneTrust Cookies Consent Notice end for vcsep.shinyapps.io/emergency_planning_tool/ -->
-
 <!-- OneTrust Cookies Settings button start -->
 <button id="ot-sdk-btn" class="ot-sdk-show-settings">Manage Cookies</button>
 <!-- OneTrust Cookies Settings button end -->
